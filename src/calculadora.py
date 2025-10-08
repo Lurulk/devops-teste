6edf9342145c4bfeb2cb68e8e8d24c659c49b062
Lurulk <j.eduardo.alvares.s@gmail.com>
2025-10-07 21:48:00 -0300

    se Deus quiser

--- src/calculadora.py
+++ src/calculadora.py
@@ -1,2 +1,8 @@
 def soma(a,b):
   return a + b
+
+def subtração(a,b):
+  return a - b
+
+def multiplicação(a,b):
+    return a * b
--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,5 +1,7 @@
-from src.calculadora import soma
+from src.calculadora import soma, subtração
 
 def test_soma():
   assert soma(2, 3) == 5
   
+def test_subtração():
+  assert subtração(5, 3) == 2
